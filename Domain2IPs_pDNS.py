8c0fba304d9c37ee328a2d9b925239cf4b470d9b
digital4rensics <keith@digital4rensics.com>
2012-11-13 18:10:19 -0500

    initial commit

---
+++ Domain2IPs_pDNS.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+# Domain2IPs_pDNS.py
+# Author: Keith Gilbert - www.digital4rensics.com - @digital4rensics
+# Version: 1.0
+# Date: November, 2012
+
+# This script will retrieve IPs associated with a given domain
+# from the ISC pDNS service. IPs will be added as entities to the graph.
+# The script requires copying the ISC provided script from ftp://ftp.isc.org/isc/nmsg/misc/isc-dnsdb-query
+# and specifying your api key in the isc-dnsdb-query.conf file.
+
+from MaltegoTransform import *
+import sys
+import subprocess
+
+badDomain = sys.argv[1]
+xform = MaltegoTransform()
+
+out = subprocess.Popen(["/bin/bash", "isc-dnsdb-query.sh", "rrset", badDomain], shell=False, stdout=subprocess.PIPE)
+results, err = out.communicate()
+
+entries = results.splitlines()
+for line in entries:
+	if line.startswith(';'):
+		pass
+	else:
+		if len(line) == 0:
+			pass
+		else:
+			IP = line.split()[3]
+			entity = xform.addEntity("maltego.IPv4Address", IP)
+	
+xform.returnOutput()
---
+++ IP2Domains_pDNS.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+# IP2Domains_pDNS.py
+# Author: Keith Gilbert - www.digital4rensics.com - @digital4rensics
+# Version: 1.0
+# Date: November, 2012
+
+# This script will retrieve domains associated with a given IP
+# from the ISC pDNS service. Domains will be added as entities to the graph.
+# The script requires copying the ISC provided script from ftp://ftp.isc.org/isc/nmsg/misc/isc-dnsdb-query
+# and specifying your api key in the isc-dnsdb-query.conf file.
+
+from MaltegoTransform import *
+import sys
+import subprocess
+
+badIP = sys.argv[1]
+xform = MaltegoTransform()
+
+out = subprocess.Popen(["/bin/bash", "isc-dnsdb-query.sh", "rdata", "ip", badIP], shell=False, stdout=subprocess.PIPE)
+results, err = out.communicate()
+
+entries = results.splitlines()
+for line in entries:
+	if len(line) == 0:
+		pass
+	else:
+		domain = line.split()[0].rstrip('.;')
+		entity = xform.addEntity("maltego.Domain", domain)
+	
+xform.returnOutput()
